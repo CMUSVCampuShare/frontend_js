0ac106cdc47edacb3ca22da6f4ffd416965e704b
WinexWolf <anukriti.jaiswal01@gmail.com>
2023-10-18 23:03:08 -0700

    added home screen

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./campushare.png";
+import "./App.css";
 
 function App() {
+  const [showContent, setShowContent] = useState(true);
+
+  // Use useEffect to hide the content after a delay
+  useEffect(() => {
+    const delay = 3000; // 3000 milliseconds (3 seconds)
+
+    const timer = setTimeout(() => {
+      setShowContent(false);
+    }, delay);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className={`App-header ${showContent ? "" : "hide-content"}`}>
+        <img src={logo} className="" alt="logo" />
+        CampuShare
       </header>
     </div>
   );
