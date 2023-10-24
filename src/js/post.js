970635435381133af0b5b307336433ae589ce5cc
ruyicS2023 <123344125+ruyicS2023@users.noreply.github.com>
2023-10-23 21:54:19 -0700

    comment dropdown fixed, post complete

--- src/js/post.js
+++ src/js/post.js
@@ -1,20 +1,14 @@
 import React, { useState } from "react";
 import "../css/post.css";
 
-function Post() {
+function Post({ author, message, availability, comments }) {
   const [showComments, setShowComments] = useState(false);
-
-  const comments = [
-    "Great! See you then.",
-    "Is there still a spot left?",
-    "I might join too!",
-  ];
 
   return (
     <div className="post-container">
       <div className="post-content">
-        <div className="post-author">ANDREW</div>
-        <div className="post-message">Hi! I'll be riding to campus at 11.</div>
+        <div className="post-author">{author}</div>
+        <div className="post-message">{message}</div>
 
         <div className="post-join-section">
           <button
@@ -32,7 +26,7 @@ function Post() {
             ))}
           </div>
 
-          <div className="post-availability">2/5</div>
+          <div className="post-availability">{availability}</div>
           <button className="join-ride-button">Join Ride</button>
         </div>
       </div>
@@ -40,4 +34,36 @@ function Post() {
   );
 }
 
-export default Post;
+function Posts() {
+  const postData = [
+    {
+      author: "ANDREW",
+      message:
+        "Hi! I'll be riding to campus at 11am. Join my ride by clicking the button below!",
+      availability: "2/5",
+      comments: ["Great! See you then.", "Is there still a spot left?"],
+    },
+    {
+      author: "EMILY",
+      message: "Riding to the downtown library at 1pm. Anyone interested?",
+      availability: "3/4",
+      comments: ["Sounds good!", "Count me in!"],
+    },
+  ];
+
+  return (
+    <div className="posts-container">
+      {postData.map((post, index) => (
+        <Post
+          key={index}
+          author={post.author}
+          message={post.message}
+          availability={post.availability}
+          comments={post.comments}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Posts;
