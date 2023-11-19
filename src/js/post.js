68cba8f492055425b7d20d525348787fd649214b
jraviS2023 <jravi@andrew.cmu.edu>
2023-11-18 17:38:27 -0800

    basic edit post logic

--- src/js/post.js
+++ src/js/post.js
@@ -14,6 +14,8 @@ const PostWall = () => {
     timestamp: new Date().toString(),
     comments: [],
   });
+  const [editMode, setEditMode] = useState(false);
+  const [editingPost, setEditingPost] = useState(null);
 
   // TO DO: Fetch posts from Springboot API
   useEffect(() => {
@@ -55,33 +57,112 @@ const PostWall = () => {
       setCommentText('');
     };
 
+    const handleEditPost = (post) => {
+      setEditMode(true);
+      setEditingPost(post);
+      setNewPostData({
+        title: post.title,
+        from: post.from,
+        to: post.to,
+        details: post.details,
+        type: post.type,
+        noOfSeats: post.noOfSeats,
+        status: post.status,
+        timestamp: post.timestamp,
+        comments: post.comments,
+      });
+    };
+
+    const handleUpdatePost = () => {
+      const updatedPosts = posts.map((p) =>
+        p.postId === editingPost.postId ? { ...newPostData, postId: p.postId } : p
+      );
+      setPosts(updatedPosts);
+      setEditMode(false);
+      setEditingPost(null);
+      setNewPostData({
+        title: '',
+        from: '',
+        to: '',
+        details: '',
+        type: '',
+        noOfSeats: 0,
+        status: 'Ongoing',
+        timestamp: new Date().toString(),
+        comments: [],
+      });
+    };
+
     return (
       <div className="post">
-        <h2>{post.title}</h2>
-        <p>From: {post.from}</p>
-        <p>To: {post.to}</p>
-        <p>Details: {post.details}</p>
-        <p>Type: {post.type}</p>
-        <p>No of Seats: {post.noOfSeats}</p>
-        <p>Status: {post.status}</p>
-        <p>Timestamp: {post.timestamp}</p>
-        <div className="comments">
-          {post.comments.map((comment, index) => (
-            <p key={index}>{comment.text}</p>
-          ))}
-          <div>
+        {editMode && editingPost.postId === post.postId ? (
+          <div className="update-post">
+            <h2>Update Existing Post</h2>
             <input
               type="text"
-              placeholder="Add a comment..."
-              value={commentText}
-              onChange={(e) => setCommentText(e.target.value)}
+              placeholder="Edit Title"
+              value={newPostData.title}
+              onChange={(e) => setNewPostData({ ...newPostData, title: e.target.value })}
             />
-            <button onClick={() => handleAddComment(post.postId)}>Comment</button>
-            <button>Join</button>
-            <button>Edit Post</button>
-            <button onClick={() => handleDeletePost(post.postId)}>Delete Post</button>
+            <input
+              type="text"
+              placeholder="Edit From"
+              value={newPostData.from}
+              onChange={(e) => setNewPostData({ ...newPostData, from: e.target.value })}
+            />
+            <input
+              type="text"
+              placeholder="Edit Deatils"
+              value={newPostData.details}
+              onChange={(e) => setNewPostData({ ...newPostData, from: e.target.value })}
+            />
+            <p>{post.type}</p>
+            <input
+              type="number"
+              placeholder="Edit Number of Seats"
+              value={newPostData.noOfSeats}
+              onChange={(e) => setNewPostData({ ...newPostData, from: e.target.value })}
+            />
+            <select
+              value={newPostData.status}
+              onChange={(e) => setNewPostData({ ...newPostData, status: e.target.value })}
+            >
+              <option value="Ongoing">Ongoing</option>
+              <option value="Complete">Complete</option>
+              <option value="Canceled">Canceled</option>
+            </select>
+            <p>{post.timestamp}</p>
+            <button onClick={handleUpdatePost}>Save Updates</button>
           </div>
-        </div>
+        ) : (
+          <div>
+            <h2>{post.title}</h2>
+            <p>From: {post.from}</p>
+            <p>To: {post.to}</p>
+            <p>Details: {post.details}</p>
+            <p>Type: {post.type}</p>
+            <p>No of Seats: {post.noOfSeats}</p>
+            <p>Status: {post.status}</p>
+            <p>Timestamp: {post.timestamp}</p>
+            <div className="comments">
+              {post.comments.map((comment, index) => (
+                <p key={index}>{comment.text}</p>
+              ))}
+              <div>
+                <input
+                  type="text"
+                  placeholder="Add a comment..."
+                  value={commentText}
+                  onChange={(e) => setCommentText(e.target.value)}
+                />
+                <button onClick={() => handleAddComment(post.postId)}>Comment</button>
+                <button>Join</button>
+                <button onClick={() => handleEditPost(post)}>Edit Post</button>
+                <button onClick={() => handleDeletePost(post.postId)}>Delete Post</button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     );
   };
