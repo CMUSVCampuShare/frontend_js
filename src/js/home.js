9b272c7b6b5f6d24da00ef7d25d684075a7a098c
Aishwarya Singh <aishwaryasingh0602@gmail.com>
2023-11-21 12:49:14 -0800

    socket front end saving code

--- src/js/home.js
+++ src/js/home.js
@@ -1,8 +1,73 @@
+import React, { useEffect, useState } from "react";
+import { over } from "stompjs";
+import SockJS from "sockjs-client";
+
+var stompClient = null;
 function Home() {
-    return (
-      <div className="">
-       
-      </div>
+  const [privateChats, setPrivateChats] = useState(new Map());
+  const [userData, setUserData] = useState({
+    username: "",
+    receivername: "",
+    connected: false,
+    message: "",
+  });
+
+  useEffect(() => {
+    console.log(userData);
+  }, [userData]);
+
+  const connect = () => {
+    let Sock = new SockJS("http://localhost:8081/ws");
+    stompClient = over(Sock);
+    stompClient.connect({}, onConnected, onError);
+  };
+
+  const onConnected = () => {
+    setUserData({ ...userData, connected: true });
+    stompClient.subscribe(
+      "/user/" + userData.username + "/notification",
+      onPrivateMessage
     );
+  };
+
+  const onPrivateMessage = (payload) => {
+    console.log(payload);
+    var payloadData = JSON.parse(payload.body);
+    if (privateChats.get(payloadData.senderName)) {
+      privateChats.get(payloadData.senderName).push(payloadData);
+      setPrivateChats(new Map(privateChats));
+    } else {
+      let list = [];
+      list.push(payloadData);
+      privateChats.set(payloadData.senderName, list);
+      setPrivateChats(new Map(privateChats));
+    }
+  };
+
+  const onError = (err) => {
+    console.log(err);
+  };
+
+  const handleUsername = (event) => {
+    const { value } = event.target;
+    setUserData({ ...userData, username: value });
+  };
+
+  const registerUser = () => {
+    connect();
+  };
+  return(<div className="register">
+            <input
+                id="user-name"
+                placeholder="Enter your name"
+                name="userName"
+                value={userData.username}
+                onChange={handleUsername}
+                margin="normal"
+              />
+              <button type="button" onClick={registerUser}>
+                    connect
+              </button> 
+        </div>);
 }
 export default Home;
