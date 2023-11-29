1a4e388aef82ab44b6bebc18b1e186de4a30c9a3
Aishwarya Singh <aishwaryasingh0602@gmail.com>
2023-11-29 13:22:41 -0800

    socket connection

--- src/js/notification.js
+++ src/js/notification.js
@@ -4,12 +4,70 @@ import SockJS from "sockjs-client";
 import { useNavigate } from "react-router-dom";
 import "../css/post.css";
 
-const userIdStored = localStorage.getItem("userId");
 var stompClient = null;
 
 function NotificationList() {
   const navigate = useNavigate();
   const [notifications, setnotifications] = useState([]);
+  const [privateChats, setPrivateChats] = useState(new Map());
+  const [userData, setUserData] = useState({
+    username: localStorage.getItem("userId"),
+    receivername: "",
+    connected: false,
+    message: "",
+  });
+
+  useEffect(() => {
+    const userIdStored = localStorage.getItem("userId");
+    console.log("connecting");
+    if (userIdStored) {
+      connect();
+    }
+  }, []);
+
+  const connect = () => {
+    let Sock = new SockJS("http://localhost:8088/ws");
+    stompClient = over(Sock);
+    stompClient.connect({}, onConnected, onError);
+  };
+
+  const onConnected = () => {
+    setUserData({ ...userData, connected: true });
+    stompClient.subscribe(
+      "/user/" + userData.username + "/notification",
+      onNotification
+    );
+  };
+
+  const onNotification = (payload) => {
+    console.log(payload);
+    var payloadData = JSON.parse(payload.body);
+
+    var answer = window.confirm(
+      "You have a new notification. Press ok to view!"
+    );
+    if (answer) {
+      var showMap = false;
+      if (
+        payloadData.notification.includes("lat") &&
+        payloadData.notification.includes("lng")
+      ) {
+        showMap = true;
+      }
+      const propsToPass = {
+        message: payloadData.notification,
+        showMap: showMap,
+        notificationId: payloadData.notificationId,
+      };
+      navigate("/join", { state: propsToPass });
+    } else {
+      console.log("notification to be viewed later");
+    }
+  };
+
+  const onError = (err) => {
+    console.log(err);
+  };
 
   useEffect(() => {
     const fetchNotifications = async () => {
@@ -32,51 +90,28 @@ function NotificationList() {
     const viewNotification = (notification) => {
       console.log(notification);
       console.log(notification.notification);
-      var showMap = false
-      if (notification.notification.includes("lat") && notification.notification.includes("lng")) {
-        showMap = true
+      var showMap = false;
+      if (
+        notification.notification.includes("lat") &&
+        notification.notification.includes("lng")
+      ) {
+        showMap = true;
       }
-      const propsToPass = { message: notification.notification, showMap: showMap, notificationId: notification.notificationId };
+      const propsToPass = {
+        message: notification.notification,
+        showMap: showMap,
+        notificationId: notification.notificationId,
+      };
       navigate("/join", { state: propsToPass });
     };
 
     return (
       <div className="post">
-        <p>{notification.notificationId}</p>
+        <p>{JSON.stringify(notification.notification)}</p>
         <button onClick={() => viewNotification(notification)}>View</button>
       </div>
     );
   };
-
-  const connect = () => {
-    let Sock = new SockJS("http://localhost:8088/ws");
-    stompClient = over(Sock);
-    //stompClient.connect({}, onConnected(), onError);
-  };
-
-  const onConnected = () => {
-    stompClient.subscribe(
-      "/user/" + userIdStored + "/notification",
-      onPrivateMessage
-    );
-  };
-
-  const onError = (err) => {
-    console.log(err);
-  };
-
-  const onPrivateMessage = (payload) => {
-    console.log(payload);
-    var result = window.confirm("Do you want to do this?");
-    console.log(payload);
-    if (result) {
-      console.log("ok");
-    } else {
-      console.log("cancel");
-    }
-  };
-
-  connect();
 
   return (
     <div className="post-wall">
