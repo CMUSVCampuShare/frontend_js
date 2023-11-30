649aed5961e5ffd9366c4ef2b04aa17cfb1f6d9a
ruyicS2023 <123344125+ruyicS2023@users.noreply.github.com>
2023-11-30 12:40:07 -0800

    add recommendation tab to navbar. recommendation ui adjustment done

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FaHome, FaComments, FaCar, FaUser } from "react-icons/fa";
+import { FaHome, FaComments, FaCar, FaUser, FaStar } from "react-icons/fa";
 import "../css/navbar.css";
 
 function Navbar() {
@@ -14,6 +14,9 @@ function Navbar() {
       </Link>
       <Link to="/post">
         <FaCar className="icon" />
+      </Link>
+      <Link to="/recommendation">
+        <FaStar className="icon" />
       </Link>
       <Link to="/profile">
         <FaUser className="icon" />
