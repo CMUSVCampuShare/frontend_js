bdc44f74f1ce3909e9ec3d980234b819d27dd320
ruyicS2023 <123344125+ruyicS2023@users.noreply.github.com>
2023-11-30 12:41:46 -0800

    remove recommendation dummy data

--- src/js/recommendation.js
+++ src/js/recommendation.js
@@ -2,51 +2,51 @@ import React, { useState, useEffect } from "react";
 import "../css/recommendation.css";
 
 const Recommendation = ({ userId }) => {
-  const topPosts = [
-    {
-      postId: "1",
-      title: "Sample Post 1",
-      from: "Location A",
-      to: "Location B",
-      details: "Details of Sample Post 1",
-      type: "Type 1",
-      noOfSeats: 3,
-      status: "ONGOING",
-      timestamp: new Date().toString(),
-    },
-  ];
-  // const [topPosts, setTopPosts] = useState([]);
-  // const [isLoading, setIsLoading] = useState(true);
-  // const [error, setError] = useState(null);
+  // const topPosts = [
+  //   {
+  //     postId: "1",
+  //     title: "Sample Post 1",
+  //     from: "Location A",
+  //     to: "Location B",
+  //     details: "Details of Sample Post 1",
+  //     type: "Type 1",
+  //     noOfSeats: 3,
+  //     status: "ONGOING",
+  //     timestamp: new Date().toString(),
+  //   },
+  // ];
+  const [topPosts, setTopPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  // useEffect(() => {
-  //   const fetchTopPostsForUser = async () => {
-  //     setIsLoading(true);
-  //     try {
-  //       const response = await fetch(
-  //         `http://localhost:8083/recommendations/user10/top-posts`
-  //       );
-  //       if (!response.ok) {
-  //         throw new Error("Could not fetch top posts");
-  //       }
-  //       const data = await response.json();
-  //       setTopPosts(data);
-  //     } catch (error) {
-  //       setError(error.message);
-  //     }
-  //     setIsLoading(false);
-  //   };
+  useEffect(() => {
+    const fetchTopPostsForUser = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(
+          `http://localhost:8083/recommendations/user10/top-posts`
+        );
+        if (!response.ok) {
+          throw new Error("Could not fetch top posts");
+        }
+        const data = await response.json();
+        setTopPosts(data);
+      } catch (error) {
+        setError(error.message);
+      }
+      setIsLoading(false);
+    };
 
-  //   fetchTopPostsForUser();
-  // }, [userId]);
+    fetchTopPostsForUser();
+  }, [userId]);
 
-  // if (isLoading) {
-  //   return <div>Loading top posts...</div>;
-  // }
+  if (isLoading) {
+    return <div>Loading top posts...</div>;
+  }
 
-  // if (error) {
-  //   return <div>Error: {error}</div>;
-  // }
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   return (
     <div className="recommendation-page">
