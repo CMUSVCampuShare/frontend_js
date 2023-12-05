f9224ed82fafcca7efc6efcf4bb7280861799234
ruyicS2023 <123344125+ruyicS2023@users.noreply.github.com>
2023-12-04 22:24:21 -0800

    profile page fetches user detail from api layer, add logout function

--- src/js/profile.js
+++ src/js/profile.js
@@ -103,15 +103,17 @@ function EditProfileModal({ isOpen, onClose, profile, onSave }) {
 
 function Profile() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [profile, setProfile] = useState({
-    username: "Jane Doe",
-    phoneNumber: "+1650776899",
-    email: "jane****@andrew.cmu.edu",
-    address: "NASA Research park",
-    role: "Driver",
-    payment: "PayPal xx24xx33xx45",
-    rewards: "100 Points",
-  });
+  const [profile, setProfile] = useState(null); // Start with no profile data
+
+  // const [profile, setProfile] = useState({
+  //   username: "Jane Doe",
+  //   phoneNumber: "+1650776899",
+  //   email: "jane****@andrew.cmu.edu",
+  //   address: "NASA Research park",
+  //   role: "Driver",
+  //   payment: "PayPal xx24xx33xx45",
+  //   rewards: "100 Points",
+  // });
 
   const handleEdit = (updatedProfile) => {
     setProfile(updatedProfile);
@@ -124,12 +126,35 @@ function Profile() {
   });
 
   useEffect(() => {
-    const userIdStored = localStorage.getItem("userId");
-    console.log("connecting");
-    if (userIdStored) {
+    const userId = localStorage.getItem("userId"); // Get userId from localStorage
+    if (userId) {
+      fetchUserProfile();
       connect();
+    } else {
+      navigate("/login"); // Redirect to login if no userId
     }
-  }, []);
+  }, [navigate]); // Depend on navigate to re-run effect if it changes
+
+  const fetchUserProfile = async () => {
+    const userId = localStorage.getItem("userId");
+    const token = localStorage.getItem("jwt"); // Get JWT token from localStorage
+
+    try {
+      const response = await fetch(`http://localhost:8080/users/${userId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (!response.ok) {
+        throw new Error("Could not fetch user data.");
+      }
+      const userProfile = await response.json();
+      setProfile(userProfile); // Set fetched profile data to state
+    } catch (error) {
+      console.error("Error fetching user profile:", error);
+      navigate("/login"); // Redirect to login on error
+    }
+  };
 
   const connect = () => {
     let Sock = new SockJS("http://localhost:8088/ws");
@@ -179,66 +204,105 @@ function Profile() {
     console.log("Error", err);
   };
 
+  const handleLogout = () => {
+    // Clear the JWT token and any other auth-related data
+    localStorage.removeItem("jwt");
+    localStorage.removeItem("userId"); // Remove userId if you're storing it
+
+    // Optionally clear any related state
+    setProfile(null);
+    setUserData({ userId: null, connected: false });
+
+    // Disconnect from WebSocket if connected
+    if (stompClient && stompClient.connected) {
+      stompClient.disconnect();
+    }
+
+    // Redirect user to login or home page
+    navigate("/login");
+  };
+
   return (
     <div className="profile-container">
-      <div className="profile-header">
-        <img
-          src="../campushare.png"
-          alt="User profile"
-          className="profile-pic"
-        />
-        <h2>{profile.username}</h2>
-        <p>{profile.email}</p>
-      </div>
+      <Navbar />
+      {profile ? ( // Only attempt to access profile properties if profile is not null
+        <>
+          <div className="profile-header">
+            <img
+              src={profile.avatarUrl || "../campushare.png"} // Use a default image if avatarUrl is not available
+              alt="User profile"
+              className="profile-pic"
+            />
+            <h2>{profile.username}</h2>
+            <p>{profile.email}</p>
+          </div>
 
-      <div className="profile-details">
-        <div className="detail-item">
-          <span>Username</span>
-          <span>{profile.username}</span>
-        </div>
-        <div className="detail-item">
-          <span>Phone Number</span>
-          <span>{profile.phoneNumber}</span>
-        </div>
-        <div className="detail-item">
-          <span>Email</span>
-          <span>{profile.email}</span>
-        </div>
-        <div className="detail-item">
-          <span>Address</span>
-          <span>{profile.address}</span>
-        </div>
-        <div className="detail-item">
-          <span>Role</span>
-          <span>{profile.role}</span>
-        </div>
-        <div className="detail-item">
-          <span>Payment</span>
-          <span>{profile.payment}</span>
-        </div>
-        <div className="detail-item">
-          <span>Rewards</span>
-          <span>{profile.rewards}</span>
-        </div>
-      </div>
+          <div className="profile-details">
+            <div className="detail-item">
+              <span>Username</span>
+              <span>{profile.username}</span>
+            </div>
+            <div className="detail-item">
+              <span>Phone Number</span>
+              <span>{profile.phoneNumber}</span>
+            </div>
+            <div className="detail-item">
+              <span>Email</span>
+              <span>{profile.email}</span>
+            </div>
+            <div className="detail-item">
+              <span>Address</span>
+              <span>{profile.address}</span>
+            </div>
+            <div className="detail-item">
+              <span>Role</span>
+              <span>{profile.role}</span>
+            </div>
+            <div className="detail-item">
+              <span>Payment</span>
+              <span>{profile.payment}</span>
+            </div>
+            <div className="detail-item">
+              <span>Rewards</span>
+              <span>{profile.rewards}</span>
+            </div>
+          </div>
 
-      <Button
-        colorScheme="red"
-        bg="#bb0000"
-        color="white"
-        size="lg"
-        _hover={{ bg: "#a00000" }}
-        onClick={() => setIsModalOpen(true)}
-      >
-        Edit
-      </Button>
+          <Button
+            colorScheme="red"
+            bg="#bb0000"
+            color="white"
+            size="lg"
+            mt={4}
+            _hover={{ bg: "#a00000" }}
+            onClick={() => setIsModalOpen(true)}
+          >
+            Edit
+          </Button>
 
-      <EditProfileModal
-        isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        profile={profile}
-        onSave={handleEdit}
-      />
+          <Button
+            colorScheme="red"
+            bg="#bb0010"
+            color="white"
+            size="lg"
+            mt={4}
+            ml={4}
+            _hover={{ bg: "#a00000" }}
+            onClick={handleLogout}
+          >
+            Logout
+          </Button>
+
+          <EditProfileModal
+            isOpen={isModalOpen}
+            onClose={() => setIsModalOpen(false)}
+            profile={profile}
+            onSave={handleEdit}
+          />
+        </>
+      ) : (
+        <Text>Loading profile...</Text> // Show a loading message while profile is null
+      )}
     </div>
   );
 }
